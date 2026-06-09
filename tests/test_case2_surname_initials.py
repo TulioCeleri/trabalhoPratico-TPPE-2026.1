43bc8b43e52075d417e04f89bba1165f431f3752
Túlio Celeri <celeritulio@terra.com.br>
2026-06-09 20:54:13 -0300

    refactor: parametrizar testes do caso 2

--- tests/test_case2_surname_initials.py
+++ tests/test_case2_surname_initials.py
@@ -4,16 +4,24 @@ from src.name_matcher import NameMatcher
 
 
 @pytest.mark.case2
-def test_surname_with_initials_without_dots_matches_full_name():
+@pytest.mark.parametrize(
+    "abbreviated_name, full_name",
+    [
+        (
+            "Seabra A M",
+            "Ana de Mattos Seabra"
+        ),
+        (
+            "Souza C.",
+            "Cassius de Souza"
+        ),
+    ]
+)
+def test_surname_with_initials_matches_full_name(
+    abbreviated_name,
+    full_name
+):
     assert NameMatcher.are_equivalent(
-        "Seabra A M",
-        "Ana de Mattos Seabra"
-    )
-
-
-@pytest.mark.case2
-def test_surname_with_dotted_initial_matches_full_name():
-    assert NameMatcher.are_equivalent(
-        "Souza C.",
-        "Cassius de Souza"
+        abbreviated_name,
+        full_name
     )
