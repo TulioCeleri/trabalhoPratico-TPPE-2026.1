b33750e0690e9d4285f2ba7bc99d5423f8a30398
Túlio Celeri <celeritulio@terra.com.br>
2026-06-09 20:58:49 -0300

    test: adicionar teste para abreviação com ponto no meio do nome

--- tests/test_case3_particles.py
+++ tests/test_case3_particles.py
@@ -9,3 +9,11 @@ def test_optional_particles_match_full_name():
         "Luiz Oliveira Souza",
         "Luiz de Oliveira de Souza"
     )
+
+
+@pytest.mark.case3
+def test_middle_initial_with_dot_matches_full_name():
+    assert NameMatcher.are_equivalent(
+        "Luiz de O. de Souza",
+        "Luiz de Oliveira de Souza"
+    )
