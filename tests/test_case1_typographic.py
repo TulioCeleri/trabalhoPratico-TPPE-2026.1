73af95ea81dac2914c97cc9bd2531694309c00b6
G0ndim <pfgondim2004@gmail.com>
2026-06-09 15:27:00 -0300

    refactor: categorizacao dos testes do caso 1 com marcador pytest

--- tests/test_case1_typographic.py
+++ tests/test_case1_typographic.py
@@ -3,37 +3,36 @@ import pytest
 from src.name_normalizer import NameNormalizer
 
 
-def test_replace_backtick_apostrophe():
-    assert (
-        NameNormalizer.normalize(
-            "Monica Hirata Sant`anna"
+@pytest.mark.case1
+class TestCase1Typographic:
+
+    def test_replace_backtick_apostrophe(self):
+        assert (
+            NameNormalizer.normalize(
+                "Monica Hirata Sant`anna"
+            )
+            ==
+            "Monica Hirata Sant'Anna"
         )
-        ==
-        "Monica Hirata Sant'Anna"
-    )
 
-
-def test_replace_curly_apostrophe():
-    assert (
-        NameNormalizer.normalize(
-            "Monica Hirata Sant’anna"
+    def test_replace_curly_apostrophe(self):
+        assert (
+            NameNormalizer.normalize(
+                "Monica Hirata Sant’anna"
+            )
+            ==
+            "Monica Hirata Sant'Anna"
         )
-        ==
-        "Monica Hirata Sant'Anna"
-    )
 
-
-def test_remove_extra_spaces():
-    assert (
-        NameNormalizer.normalize(
-            "Monica    Hirata    Sant`anna"
+    def test_remove_extra_spaces(self):
+        assert (
+            NameNormalizer.normalize(
+                "Monica    Hirata    Sant`anna"
+            )
+            ==
+            "Monica Hirata Sant'Anna"
         )
-        ==
-        "Monica Hirata Sant'Anna"
-    )
 
-
-def test_empty_name():
-
-    with pytest.raises(ValueError):
-        NameNormalizer.normalize("")
+    def test_empty_name(self):
+        with pytest.raises(ValueError):
+            NameNormalizer.normalize("")
