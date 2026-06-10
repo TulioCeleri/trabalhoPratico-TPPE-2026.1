db803eed75e2f7794bdf602e01529836cf9c22a6
guinuto2 <guiguievangelista22@gmail.com>
2026-06-09 23:39:26 -0300

    feat: criar deduplicador de autores

---
+++ src/author_deduplicator.py
@@ -0,0 +1,20 @@
+from src.name_matcher import NameMatcher
+
+
+class AuthorDeduplicator:
+
+    @staticmethod
+    def deduplicate(authors):
+        if len(authors) < 2:
+            return authors
+
+        first_author = authors[0]
+        second_author = authors[1]
+
+        if NameMatcher.are_equivalent(
+            first_author.name,
+            second_author.name
+        ):
+            return [first_author]
+
+        return authors
