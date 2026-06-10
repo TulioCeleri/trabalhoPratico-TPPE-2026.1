3ec1ea977b63b40b267027c3f3a7602b1dc8b2dd
Túlio Celeri <celeritulio@terra.com.br>
2026-06-09 21:01:17 -0300

    refactor: parametrizar testes do caso 3

--- tests/test_case3_particles.py
+++ tests/test_case3_particles.py
@@ -4,16 +4,24 @@ from src.name_matcher import NameMatcher
 
 
 @pytest.mark.case3
-def test_optional_particles_match_full_name():
+@pytest.mark.parametrize(
+    "variant_name, full_name",
+    [
+        (
+            "Luiz Oliveira Souza",
+            "Luiz de Oliveira de Souza"
+        ),
+        (
+            "Luiz de O. de Souza",
+            "Luiz de Oliveira de Souza"
+        ),
+    ]
+)
+def test_particles_and_dots_are_optional(
+    variant_name,
+    full_name
+):
     assert NameMatcher.are_equivalent(
-        "Luiz Oliveira Souza",
-        "Luiz de Oliveira de Souza"
-    )
-
-
-@pytest.mark.case3
-def test_middle_initial_with_dot_matches_full_name():
-    assert NameMatcher.are_equivalent(
-        "Luiz de O. de Souza",
-        "Luiz de Oliveira de Souza"
+        variant_name,
+        full_name
     )
